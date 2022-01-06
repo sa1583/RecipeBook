9b0de70633016db0f36c600e5aa3c16fdaa77413
sa1583 <sa1583@naver.com>
2022-01-06 22:46:39 +0900

    각 Fragment 이름 수정

--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.recipeListFragment, R.id.recipeDetailFragment, R.id.addRecipeFragment
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
