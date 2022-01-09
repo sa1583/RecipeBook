e118fc3ee23ce316ff2437f3d80e09c5e08da469
sa1583 <sa1583@naver.com>
2022-01-09 14:19:16 +0900

    ADD recipe 추가 화면 추가

---
+++ app/src/main/java/com/example/recipebook/ui/add/AddRecipeFragment.kt
@@ -0,0 +1,37 @@
+package com.example.recipebook.ui.add
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.recipebook.databinding.FragmentAddRecipeBinding
+
+class AddRecipeFragment : Fragment() {
+
+    private lateinit var mAddRecipeViewModel: AddRecipeViewModel
+    private var _binding: FragmentAddRecipeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddRecipeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
