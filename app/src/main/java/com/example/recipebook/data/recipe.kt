4cd570aaec73efce0ed119a2cfcb48a0e3e4114f
sa1583 <sa1583@naver.com>
2022-01-12 17:22:34 +0900

    FIX Entity 구조 수정

--- app/src/main/java/com/example/recipebook/data/recipe.kt
+++ app/src/main/java/com/example/recipebook/data/recipe.kt
@@ -1,21 +1,22 @@
 package com.example.recipebook.data
 
+import android.net.Uri
 import androidx.room.*
 
 @Entity(tableName = "recipe")
 data class Recipe(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "name") val recipeName: String,
-    @ColumnInfo(name = "image") val recipeImage: Int
+    @ColumnInfo(name = "image") val recipeImage: Uri
 )
 
 @Entity(tableName = "Ingredient")
 data class Ingredient(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "recipe_id") val recipeId: Int,
     @ColumnInfo(name = "name") val ingredientName: String,
     @ColumnInfo(name = "amount") val ingredientAmount: Int,
-    @ColumnInfo(name = "unit") val ingredientUnit: String
+    @ColumnInfo(name = "unit") val ingredientUnit: Int
 )
 
 data class RecipeWithIngredients(
