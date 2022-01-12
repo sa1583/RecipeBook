341e63a3f1219f0ce4c8370a801c5ba691f2ee5c
sa1583 <sa1583@naver.com>
2022-01-12 17:21:46 +0900

    ADD 레시피 등록을 위한 로직 추가

--- app/src/main/java/com/example/recipebook/ui/add/AddRecipeViewModel.kt
+++ app/src/main/java/com/example/recipebook/ui/add/AddRecipeViewModel.kt
@@ -1,13 +1,81 @@
 package com.example.recipebook.ui.add
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.net.Uri
+import androidx.lifecycle.*
+import com.example.recipebook.data.Ingredient
+import com.example.recipebook.data.Recipe
+import com.example.recipebook.data.RecipeDao
+import com.example.recipebook.data.RecipeWithIngredients
+import kotlinx.coroutines.launch
 
-class AddRecipeViewModel : ViewModel() {
+class AddRecipeViewModel(private val recipeDao: RecipeDao) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private fun getNewRecipe(recipeName: String, recipeImage: Uri): Recipe {
+        return Recipe(
+            recipeName = recipeName,
+            recipeImage = recipeImage
+        )
     }
-    val text: LiveData<String> = _text
+
+    private fun getNewIngredient(
+        recipeId: Int,
+        ingredientName: String,
+        ingredientAmount: Int,
+        ingredientAmountUnit: Int
+    ): Ingredient {
+        return Ingredient(
+            recipeId = recipeId,
+            ingredientName = ingredientName,
+            ingredientAmount = ingredientAmount,
+            ingredientUnit = ingredientAmountUnit
+        )
+    }
+
+    private fun insertRecipeAndIngredient(
+        recipe: Recipe,
+        ingredientNames: List<String>,
+        ingredientAmounts: List<Int>,
+        ingredientAmountUnits: List<Int>
+    ) {
+        viewModelScope.launch {
+            val id = recipeDao.addRecipe(recipe)
+            val ingredients = mutableListOf<Ingredient>()
+            for (i: Int in ingredientNames.indices) {
+                ingredients.add(
+                    getNewIngredient(
+                        id,
+                        ingredientNames[i],
+                        ingredientAmounts[i],
+                        ingredientAmountUnits[i]
+                    )
+                )
+            }
+            recipeDao.addIngredients(ingredients)
+        }
+    }
+
+    fun retrieveRecipe(id: Int): LiveData<RecipeWithIngredients> {
+        return recipeDao.getRecipeWithIngredients(id).asLiveData()
+    }
+
+    fun addNewRecipe(
+        recipeName: String,
+        recipeImage: Uri,
+        ingredientNames: List<String>,
+        ingredientAmounts: List<Int>,
+        ingredientAmountUnits: List<Int>
+    ) {
+        val recipe = getNewRecipe(recipeName, recipeImage)
+        insertRecipeAndIngredient(recipe, ingredientNames, ingredientAmounts, ingredientAmountUnits)
+    }
+}
+
+class AddRecipeViewModelFactory(private val recipeDao: RecipeDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AddRecipeViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AddRecipeViewModel(recipeDao) as T
+        }
+        throw IllegalArgumentException("Unknown viewModel class")
+    }
 }
